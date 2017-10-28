b72b908d64946a72bda9816571e45986144b9183
gina <gina.a.john@gmail.com>
2017-10-28 20:40:17 +0530

    allowed_hosts change

--- testDinero/settings.py
+++ testDinero/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(fm8t)@cl9=q#*^wuoe9hgs=)m_1qh5mhfl+a6p=0jn2ennyn8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
